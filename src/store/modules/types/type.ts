5cd9553d1941769935e11445a815cfe925c76a33
youthOfMe <204069080@qq.com>
2024-01-27 20:39:08 +0800

    feat: 配置登录页面的校验功能

--- src/store/modules/types/type.ts
+++ src/store/modules/types/type.ts
@@ -1,4 +1,3 @@
-import type { RouteRecordRaw } from 'vue-router'
 // 定义小仓库的数据类型
 export interface UserState {
     token: string | null
--- src/views/login/Index.vue
+++ src/views/login/Index.vue
@@ -3,17 +3,31 @@
         <el-row>
             <el-col :span="12" :xs="0">占位的格子</el-col>
             <el-col :span="12" :xs="24">
-                <el-form class="login_form" :model="loginForm">
+                <el-form
+                    class="login_form"
+                    :model="loginForm"
+                    :rules="rules"
+                    ref="loginForms"
+                >
                     <h1>Hello</h1>
                     <h2>欢迎来到牛马商城后台管理系统</h2>
-                    <el-form-item>
+                    <el-form-item prop="username">
                         <el-input v-model="loginForm.username"></el-input>
                     </el-form-item>
-                    <el-form-item>
-                        <el-input></el-input>
+                    <el-form-item prop="password">
+                        <el-input
+                            type="password"
+                            v-model="loginForm.password"
+                            show-password
+                        ></el-input>
                     </el-form-item>
-                    <el-form-item v-model="loginForm.password">
-                        <el-button class="login_btn" type="primary">
+                    <el-form-item>
+                        <el-button
+                            class="login_btn"
+                            type="primary"
+                            @click="login()"
+                            :loading="loading"
+                        >
                             登录
                         </el-button>
                     </el-form-item>
@@ -24,17 +38,72 @@
 </template>
 
 <script setup lang="ts">
-import { useTransitionFallthroughEmits } from 'element-plus'
 import { reactive } from 'vue'
 import { ref } from 'vue'
+// 引入用户相关的仓库
+import useUserStore from '@/store/modules/user'
+// 引入路由对象
+import { useRouter } from 'vue-router'
+import { useRoute } from 'vue-router'
+// 引入获取当前时间的函数
+import { getTime } from '@/utils/time'
+// 引入element
+import { ElNotification } from 'element-plus'
 
+// 创建用户仓库进行使用
+const userStore = useUserStore()
+// 获取路由对象
+const $route = useRoute()
+// 获取全局路由对象
+const $router = useRouter()
 // 收集账号密码
 const loginForm = reactive({ username: 'admin', password: '111111' })
+// 控制按钮的加载效果
+const loading = ref<boolean>(false)
+// 获取表单对象
+const loginForms = ref()
+// 配置自定义校验的方法
+const validatorUserName = (rule: object, value: string, callback: any) => {
+    if (value.length >= 5 && value.length <= 18) callback()
+    else callback(new Error('账号长度要在5-18位之间'))
+}
+const validatorPassword = (rule: object, value: string, callback: any) => {
+    if (value.length >= 5 && value.length <= 18) callback()
+    else callback(new Error('密码长度要在6-18位之间'))
+}
+// 定义表单校验需要进行配置的对象
+const rules = {
+    // 自定义校验规则
+    username: [{ trigger: 'change', validator: validatorUserName }],
+    password: [{ trigger: 'change', validator: validatorPassword }],
+}
 // 登录按钮的回调
 const login = async () => {
+    // 保证表单校验通过再发送请求
+    await loginForms.value.validate()
+    // 开始加载
+    loading.value = true
     // 处理登录请求
-    // try {
-    // }
+    try {
+        // 保证登录成功
+        await userStore.userLogin(loginForm)
+        // 跳转到首页
+        const redirect = $route.query.redirect
+        $router.push({ path: (redirect as string) || '/' })
+        // 登录成功后的提示信息
+        ElNotification({
+            type: 'success',
+            title: `Hi, ${getTime()}好`,
+            message: '您辛苦了, 管理员',
+        })
+    } catch (error) {
+        ElNotification({
+            type: 'error',
+            message: (error as Error).message,
+        })
+    }
+    // 无论是否失败都要改变按钮的状态
+    loading.value = false
 }
 </script>
 
