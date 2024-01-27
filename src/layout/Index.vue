cd4726f6c1f0a139aa4b5352588fa87299e4df94
youthOfMe <204069080@qq.com>
2024-01-27 21:24:26 +0800

    feat: 配置项目layout页面的通用样式变量 结合sass”

--- src/layout/Index.vue
+++ src/layout/Index.vue
@@ -1,7 +1,23 @@
 <template>
-    <div></div>
+    <div class="layout_container">
+        <!-- 左侧菜单 -->
+        <div class="layout_slider"></div>
+        <!-- 顶部导航 -->
+        <div class="layout_tabBar"></div>
+        <!-- 内容展示区域 -->
+        <div class="layout_main"></div>
+    </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.layout_container {
+    width: 100%;
+    height: 100vh;
+}
+
+.layout_slider {
+    color: white;
+}
+</style>
