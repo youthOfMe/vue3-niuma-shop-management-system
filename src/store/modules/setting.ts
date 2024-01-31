5d988b00b193cb09a5f2b3af5a459733b23cf404
youthOfMe <204069080@qq.com>
2024-01-31 16:07:09 +0800

    feat: 准备layout页面配置仓库“

---
+++ src/store/modules/setting.ts
@@ -0,0 +1,11 @@
+import { defineStore } from 'pinia'
+// 创建layout组件相关的配置仓库
+
+const useLayOutSettingStore = defineStore('SettingStore', {
+    state: () => {
+        return {
+            fold: false,
+            refresh: false,
+        }
+    },
+})
