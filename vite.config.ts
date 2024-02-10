4cae1b0f35451995fad523bf57e85a7c2d57b5e3
youthOfMe <2040690180@qq.com>
2024-02-10 19:49:23 +0800

    feat: 配置不同环境下的目标服务器地址 配置代理跨域

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
@@ -6,7 +6,9 @@ import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command, mode }) => {
+    // 获取对应的开发环境 方便加载不同开发环境的配置信息
+    const env = loadEnv(mode, process.cwd())
     return {
         plugins: [
             vue(),
@@ -30,6 +32,19 @@ export default defineConfig(({ command }) => {
                 scss: {
                     javascriptEnabled: true,
                     additionalData: '@import "./src/styles/variable.scss";',
+                },
+            },
+        },
+        // 代理跨域
+        server: {
+            proxy: {
+                [env.VITE_APP_BASE_API]: {
+                    // 获取数据的服务器地址设置
+                    target: env.VITE_SERVE,
+                    // 是否需要代理服务器跨域
+                    changeOrigin: true,
+                    // 路径重写 将匹配到的路径中的/api替换为空串
+                    rewrite: (path) => path.replace(/^\/api/, ''),
                 },
             },
         },
