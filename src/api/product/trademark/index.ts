32a5b99d425a577e69ec25d14bab3e366e331356
youthOfMe <2040690180@qq.com>
2024-02-10 20:28:26 +0800

    feat: 准备添加与修改已有品牌接口方法

--- src/api/product/trademark/index.ts
+++ src/api/product/trademark/index.ts
@@ -1,6 +1,6 @@
 // 品牌管理模块接口
 import request from '@/utils/request'
-import type { TradeMarkResponseData } from './type'
+import type { TradeMark, TradeMarkResponseData } from './type'
 // 品牌接口地址
 enum API {
     // 获取已有品牌的接口地址
@@ -14,3 +14,14 @@ enum API {
 // 获取已有品牌的接口
 export const reqHasTrademark = (page: number, limit: number) =>
     request.get<any, TradeMarkResponseData>(API.TRADEMARK_URL + `/${page}/${limit}`)
+
+// 添加与修改已有品牌接口方法
+export const reqAddOrUpdateTrademark = (data: TradeMark) => {
+    if (data.id) {
+        // 修改已有品牌的数据
+        return request.put<any, any>(API.UPDATETRADEMARK_URL, data)
+    } else {
+        // 新增品牌
+        return request.post<any, any>(API.ADDTRADEMARK_URL, data)
+    }
+}
